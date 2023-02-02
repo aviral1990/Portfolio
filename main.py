faab5586c5bcbc16e54975c9e5ea5837642cc776
Aviral-PC <aviral17@hotmail.com>
2023-02-02 21:01:13 +0530

    Added Columns, pic and about me

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 import streamlit as st
 
+st.set_page_config(page_title="Portfolio",layout="wide")
+
+
+col1,col2=st.columns(2)
+
+
+
+with col1:
+    st.image("images/photo.jpg")
+    st.subheader("TO-Do App")
+
+about_me_str="""
+Hello, this is Aviral. I enjoy coding and am passionate about developing firmwares for hardware 
+prototypes as well as developing software applications using python.
+"""
+
+with col2:
+    st.title("About Me")
+    st.info(about_me_str)
