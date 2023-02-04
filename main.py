83ae59d015983c1814559056d48f94e337bd4a94
Aviral-PC <aviral17@hotmail.com>
2023-02-04 13:04:16 +0530

    Added App description and links, adjusted column widths

--- main.py
+++ main.py
@@ -26,17 +26,25 @@ with col2:
     st.info(about_me_str)
 
 
-st.header("You can find the applications that I have coded using python")
+st.subheader("You can find the applications that I have coded using python")
+st.subheader("")
+st.subheader("")
 
 #declare 2 new columns
-col3,col4=st.columns(2)
+col3,empty_col,col4=st.columns([1.5,0.5,1.5])     #Ratio of column width
 
 df=pandas.read_csv("data.csv",sep=";")      #data being read separated by ;
 
 with col3:
     for index, row in df[0:10].iterrows():        #Print first 10 rows of csv with title
         st.subheader(row["title"])
+        st.write(row["description"])              #Print descriptions from csv
+        st.image("images/" + row["image"])        #in directory images, title image in csv
+        st.write(f"[Source Code]({row['url']})")  #Label the link from csv as source code
 
 with col4:
     for index, row in df[10:].iterrows():        #Print next 10 rows of csv with title
         st.subheader(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])  # in directory images, title image in csv
+        st.write(f"[Source Code]({row['url']})")  # Label the link from csv as source code
