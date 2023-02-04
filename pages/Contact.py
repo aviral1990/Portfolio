e5a5a34ecba87678b4e607548ebf42f64bebcacf
Aviral-PC <aviral17@hotmail.com>
2023-02-04 19:55:53 +0530

    Added Contact page

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.title("Contact Me")
+
+with st.form(key="form"):         #Create a form
+    user_email=st.text_input(label="Your Email")
+    message=st.text_area("Your Message")
+    button=st.form_submit_button(label="submit")
+    if button:      #Send email
+
+
+
