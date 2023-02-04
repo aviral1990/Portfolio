1f0beb0becbf30526f5464c01cd50b45d4b31ab9
Aviral-PC <aviral17@hotmail.com>
2023-02-04 20:32:44 +0530

    Email connected to webpage

--- pages/Contact.py
+++ pages/Contact.py
@@ -5,9 +5,12 @@ st.title("Contact Me")
 
 with st.form(key="form"):         #Create a form
     user_email=st.text_input(label="Your Email")
-    message=st.text_area("Your Message")
+    message=st.text_area("Your Message",key="text_area")
     button=st.form_submit_button(label="submit")
     if button:      #Send email
-        send_email.send_email("Hello")
+        send_email.send_email("Subject: Portfolio Webpage"+"\n"+message+"\n"+user_email)
+        st.info("Email sent successfully")
 
 
+
+print(message)
