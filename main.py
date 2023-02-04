d655857132097821a731adc11f57b8eb6e62a5e3
Aviral-PC <aviral17@hotmail.com>
2023-02-04 12:46:59 +0530

    Added App titles under columns

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import pandas
 import streamlit as st
+import pandas as pd
+def Row_gap(gap):
+    while (gap):
+        st.write("")
+        gap=gap-1
 
 st.set_page_config(page_title="Portfolio",layout="wide")
 
@@ -9,7 +15,6 @@ col1,col2=st.columns(2)
 
 with col1:
     st.image("images/photo.jpg")
-    st.subheader("TO-Do App")
 
 about_me_str="""
 Hello, this is Aviral. I enjoy coding and am passionate about developing firmwares for hardware 
@@ -19,3 +24,19 @@ prototypes as well as developing software applications using python.
 with col2:
     st.title("About Me")
     st.info(about_me_str)
+
+
+st.header("You can find the applications that I have coded using python")
+
+#declare 2 new columns
+col3,col4=st.columns(2)
+
+df=pandas.read_csv("data.csv",sep=";")      #data being read separated by ;
+
+with col3:
+    for index, row in df[0:10].iterrows():        #Print first 10 rows of csv with title
+        st.subheader(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():        #Print next 10 rows of csv with title
+        st.subheader(row["title"])
